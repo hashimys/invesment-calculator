6f5dd02f7dd3a79e09c6b5f7a8c92fb8d03af07f
hashimys <hashimys@gmail.com>
2025-08-20 13:46:32 +0300

    chore: upgrade Angular dependencies to version 20 and TypeScript to 5.8.0
    feat: add UserInput component for user investment input form
    
    style: add CSS styles for UserInput component
    
    fix: update app.component to include UserInput component in imports and template

---
+++ src/user-input/user-input.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [FormsModule],
+  templateUrl: './user-input.html',
+  styleUrl: './user-input.css'
+})
+export class UserInput {
+
+  onSubmit() {
+    console.log('hello');
+  }
+}
