29b1c7a9d269e5250e395f5f4a7bda98590ca9ba
hashimys <hashimys@gmail.com>
2025-08-20 14:09:35 +0300

    feat: enhance user input form with two-way data binding and improved button hover effect

--- src/user-input/user-input.ts
+++ src/user-input/user-input.ts
@@ -5,11 +5,19 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-user-input',
   imports: [FormsModule],
   templateUrl: './user-input.html',
-  styleUrl: './user-input.css'
+  styleUrl: './user-input.css',
 })
 export class UserInput {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '0';
+  enteredDuration = '0';
 
   onSubmit() {
     console.log('hello');
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 }
