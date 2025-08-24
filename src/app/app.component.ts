8699f955f7eeca1ef29c0990dac60f81e7348d0c
hashimys <hashimys@gmail.com>
2025-08-24 14:29:14 +0300

    feat: implement investment calculator with user input, results display, and service integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
-import { UserInput } from '../user-input/user-input';
+import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
+import { InvestmentResults } from './investment-results/investment-results';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [Header, UserInput],
+  imports: [Header, UserInput, InvestmentResults],
 })
-export class AppComponent {}
+export class AppComponent {
+}
